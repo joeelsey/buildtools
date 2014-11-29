df8c3cf0ad2b5d26f92e5f9ecc5af744f3e2cb63
Joe Elsey <joe.elsey@gmail.com>
2014-11-29 13:41:09 -0800

    html woes

--- public/index.js
+++ public/index.js
@@ -1,10 +1,10 @@
-var $ = require('jquery');
+//var $ = require('jquery');
 
 $(document).on('ready', function(){
   $.ajax({
     type: 'GET',
     dataType: 'text',
-    url: 'http://jsonip.com',
+    url: 'http://localhost:3000/ip',
     success: function(data){
       $('body').html(data);
     }
