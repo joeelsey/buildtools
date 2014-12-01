4a13d240d1101ed94b8e27ae3a5f0aa1205a127c
Joe Elsey <joe.elsey@gmail.com>
2014-12-01 10:39:24 -0800

    shldkfhl

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var $ = require('jquery');
+//var $ = require('jquery');
 
 $(document).on('ready', function(){
   $.ajax({
--- server.js
+++ server.js
@@ -3,24 +3,30 @@
 var express = require('express');
 var app = express();
 var bodyparser = require('body-parser');
+var request = require('superagent');
 
 app.use(bodyparser.json());
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.get('/ip', function(req, res) {
+  var jsonip = 'http://jsonip.com';
+
+  request
+    .get(jsonip)
+    .end(function(err, jsondata) {
+      if (err) res.status(500).send(err);
+      var parsedData = JSON.parse(jsondata.text);
+      var ip = parsedData.ip;
+      res.json('Your ip address is: ' + ip);
+    });
+});
+
 app.use(express.static(__dirname + '/build'));
-
-var request = require('superagent');
-  
-  app.get('/ip', function(req, res) {
-    var jsonip = 'http://jsonip.com';
-
-    request
-      .get(jsonip)
-      .end(function(err, jsondata) {
-        if (err) res.status(500).send(err);
-        var parsedData = JSON.parse(jsondata.text);
-        var ip = parsedData.ip;
-        res.json('Your ip address is: ' + ip);
-      });
-  });
 
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function() {
